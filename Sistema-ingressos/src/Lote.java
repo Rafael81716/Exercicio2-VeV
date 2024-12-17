a23d2a3864b0ad49706ad01f406c63e6ac2aed03
Ingrid <ingrid.castro@ccc.ufcg.edu.br>
2024-12-16 21:44:42 -0300

    feat: create Lote class

---
+++ Sistema-ingressos/src/Lote.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+
+
+public class Lote {
+
+	private int id;
+	private Set<Ingresso> ingressos;
+	private double desconto;
+	
+	public Lote(int id, Set<Ingresso> ingressos, double desconto) {
+		this.id = id;
+		this.ingressos = ingressos;
+		this.desconto = desconto;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public Set<Ingresso> getIngressos() {
+		return this.ingressos;
+	}
+	
+	public double getDesconto() {
+		return this.desconto;
+	}
+	
+}
