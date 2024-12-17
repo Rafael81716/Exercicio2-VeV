35563d6016e5874ffcd30896f576f732c586093e
Ingrid <ingrid.castro@ccc.ufcg.edu.br>
2024-12-16 21:33:21 -0300

    feat: add tests about set Ingresso status funcionality

--- Sistema-ingressos/test/IngressoTest.java
+++ Sistema-ingressos/test/IngressoTest.java
@@ -39,5 +39,23 @@ class IngressoTest {
 		assertEquals(ingresso.getTipo(), tipo);
 		assertTrue(ingresso.getStatus());
 	}
+	
+	@Test
+	void testMarcaIngressoComoVendido() {
+		Ingresso ingresso = new Ingresso(4, TipoIngresso.NORMAL, false);
+		
+		ingresso.setStatus(true);
+		
+		assertTrue(ingresso.getStatus());
+	}
+	
+	@Test
+	void testMarcaIngressoComoNaoVendido() {
+		Ingresso ingresso = new Ingresso(5, TipoIngresso.VIP, true);
+		
+		ingresso.setStatus(false);
+		
+		assertFalse(ingresso.getStatus());
+	}
 
 }
