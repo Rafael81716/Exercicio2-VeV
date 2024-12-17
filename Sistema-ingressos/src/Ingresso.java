9fb65e1b0ac5a1cb13ba4ffa52d7cd233ee8a8db
Ingrid <ingrid.castro@ccc.ufcg.edu.br>
2024-12-16 21:31:04 -0300

    feat: create Ingresso class and TipoIngresso enum

---
+++ Sistema-ingressos/src/Ingresso.java
@@ -0,0 +1,25 @@
+public class Ingresso {
+
+	private int id;
+	private TipoIngresso tipo;
+	private boolean statusVendido;
+	
+	public Ingresso(int id, TipoIngresso tipo, boolean statusVendido) {
+		this.id = id;
+		this.tipo = tipo;
+		this.statusVendido = statusVendido;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public TipoIngresso getTipo() {
+		return this.tipo;
+	}
+	
+	public boolean getStatus() {
+		return this.statusVendido;
+	}
+	
+}
