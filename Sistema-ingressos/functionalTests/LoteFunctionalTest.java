bfa375ad92dab3eda9da781ea063f3244dc4373e
Ingrid Castro <ingrid.castro@ccc.ufcg.edu.br>
2025-02-23 11:13:49 -0300

    fix: adjust functional tests for Sistema de Ingressos

--- Sistema-ingressos/functionalTests/LoteFunctionalTest.java
+++ Sistema-ingressos/functionalTests/LoteFunctionalTest.java
@@ -1,109 +1,117 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 
 class LoteFunctionalTest {
-
-	@Test
-	void testCriaLoteComDescontoNegativo() {
-		int id = 1;
-		double desconto = -0.01;
+	
+	@Nested
+	class LoteFunctionalTestAVL {
 		
-		assertThrows(IllegalArgumentException.class, () -> new Lote(id, desconto));
-	}
-	
-	@Test
-	void testCriaLoteSemDesconto() {
-		int id = 2;
-		double desconto = 0;
-		Lote lote = new Lote(id, desconto);
+		@Test
+		void testCriaLoteComDescontoNegativo() {
+			int id = 1;
+			double desconto = -0.01;
 			
-		assertEquals(lote.getDesconto(), desconto);
+			assertThrows(IllegalArgumentException.class, () -> new Lote(id, desconto));
+		}
+		
+		@Test
+		void testCriaLoteSemDesconto() {
+			int id = 2;
+			double desconto = 0;
+			Lote lote = new Lote(id, desconto);
+				
+			assertEquals(lote.getDesconto(), desconto);
+		}
+		
+		@Test
+		void testCriaLoteComDescontoMinimo() {
+			int id = 3;
+			double desconto = 0.01;
+			Lote lote = new Lote(id, desconto);
+				
+			assertEquals(lote.getDesconto(), desconto);
+		}
+		
+		@Test
+		void testCriaLoteComDescontoQualquerPermitido() {
+			int id = 4;
+			double desconto = 0.13;
+			Lote lote = new Lote(id, desconto);
+				
+			assertEquals(lote.getDesconto(), desconto);
+		}
+		
+		@Test
+		void testCriaLoteComDescontoAbaixoMaximo() {
+			int id = 5;
+			double desconto = 0.24;
+			Lote lote = new Lote(id, desconto);
+				
+			assertEquals(lote.getDesconto(), desconto);
+		}
+		
+		@Test
+		void testCriaLoteComDescontoMaximo() {
+			int id = 6;
+			double desconto = 0.25;
+			Lote lote = new Lote(id, desconto);
+				
+			assertEquals(lote.getDesconto(), desconto);
+		}
+		
+		@Test
+		void testCriaLoteComDescontoAcimaMaximo() {
+			int id = 7;
+			double desconto = 0.26;
+				
+			assertThrows(IllegalArgumentException.class, () -> new Lote(id, desconto));
+		}
 	}
-	
-	@Test
-	void testCriaLoteComDescontoMinimo() {
-		int id = 3;
-		double desconto = 0.01;
-		Lote lote = new Lote(id, desconto);
-			
-		assertEquals(lote.getDesconto(), desconto);
-	}
-	
-	@Test
-	void testCriaLoteComDescontoQualquerPermitido() {
-		int id = 4;
-		double desconto = 0.13;
-		Lote lote = new Lote(id, desconto);
-			
-		assertEquals(lote.getDesconto(), desconto);
-	}
-	
-	@Test
-	void testCriaLoteComDescontoAbaixoMaximo() {
-		int id = 5;
-		double desconto = 0.24;
-		Lote lote = new Lote(id, desconto);
-			
-		assertEquals(lote.getDesconto(), desconto);
-	}
-	
-	@Test
-	void testCriaLoteComDescontoMaximo() {
-		int id = 6;
-		double desconto = 0.25;
-		Lote lote = new Lote(id, desconto);
-			
-		assertEquals(lote.getDesconto(), desconto);
-	}
-	
-	@Test
-	void testCriaLoteComDescontoAcimaMaximo() {
-		int id = 7;
-		double desconto = 0.26;
-			
-		assertThrows(IllegalArgumentException.class, () -> new Lote(id, desconto));
-	}
-	
-	@Test
-	void testCalculaCustoIngressoSemDesconto() {
-		Lote lote = new Lote(8, 0);
-		lote.adicionaIngresso(new Ingresso(1, TipoIngresso.NORMAL, false));
-		Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
-		double custoIngressoNormal = 100;
-			
-		assertEquals(ingresso.calculaCusto(custoIngressoNormal), custoIngressoNormal);
-	}
-	
-	@Test
-	void testCalculaCustoIngressoNormalComDesconto() {
-		Lote lote = new Lote(9, 0.15);
-		lote.adicionaIngresso(new Ingresso(2, TipoIngresso.NORMAL, false));
-		Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
-		double custoIngressoNormal = 100;
-			
-		assertEquals(ingresso.calculaCusto(custoIngressoNormal), 85);
-	}
-	
-	@Test
-	void testCalculaCustoIngressoVipComDesconto() {
-		Lote lote = new Lote(10, 0.15);
-		lote.adicionaIngresso(new Ingresso(3, TipoIngresso.VIP, false));
-		Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
-		double custoIngressoNormal = 50;
-			
-		assertEquals(ingresso.calculaCusto(custoIngressoNormal), 85);
-	}
-	
-	@Test
-	void testCalculaCustoIngressoMeiaEntradaComDesconto() {
-		Lote lote = new Lote(11, 0.15);
-		lote.adicionaIngresso(new Ingresso(4, TipoIngresso.MEIA_ENTRADA, false));
-		Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
-		double custoIngressoNormal = 200;
-			
-		assertEquals(ingresso.calculaCusto(custoIngressoNormal), 100);
+
+	@Nested
+	class LoteFunctionalTestTabelaDecisao {
+		@Test
+		void testCalculaCustoIngressoSemDesconto() {
+			Lote lote = new Lote(8, 0);
+			lote.adicionaIngresso(new Ingresso(1, TipoIngresso.NORMAL, false));
+			Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
+			double custoIngressoNormal = 100;
+				
+			assertEquals(ingresso.calculaCusto(custoIngressoNormal), custoIngressoNormal);
+		}
+		
+		@Test
+		void testCalculaCustoIngressoNormalComDesconto() {
+			Lote lote = new Lote(9, 0.15);
+			lote.adicionaIngresso(new Ingresso(2, TipoIngresso.NORMAL, false));
+			Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
+			double custoIngressoNormal = 100;
+				
+			assertEquals(ingresso.calculaCusto(custoIngressoNormal), 85);
+		}
+		
+		@Test
+		void testCalculaCustoIngressoVipComDesconto() {
+			Lote lote = new Lote(10, 0.15);
+			lote.adicionaIngresso(new Ingresso(3, TipoIngresso.VIP, false));
+			Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
+			double custoIngressoNormal = 50;
+				
+			assertEquals(ingresso.calculaCusto(custoIngressoNormal), 85);
+		}
+		
+		@Test
+		void testCalculaCustoIngressoMeiaEntradaComDesconto() {
+			Lote lote = new Lote(11, 0.15);
+			lote.adicionaIngresso(new Ingresso(4, TipoIngresso.MEIA_ENTRADA, false));
+			Ingresso ingresso = (Ingresso) lote.getIngressos().toArray()[0];
+			double custoIngressoNormal = 200;
+				
+			assertEquals(ingresso.calculaCusto(custoIngressoNormal), 100);
+		}
 	}
 	
 }
--- Sistema-ingressos/functionalTests/ShowFunctionalTest.java
+++ Sistema-ingressos/functionalTests/ShowFunctionalTest.java
@@ -3,87 +3,103 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 
 class ShowFunctionalTest {
 	
-	private Map<Integer, Lote> lotesIngressos;
-	
-	@BeforeEach
-	void initLoteIngressos() {
-		this.lotesIngressos = new HashMap<>();
+	@Nested
+	class ShowFunctionalTestAVL {
+		private Map<Integer, Lote> lotesIngressos;
 		
-		this.lotesIngressos.put(1, new Lote(1, 0.25));
-	}
-	
-	@Test
-	void testDistribuiIngressosVip() {
-		Show show = new Show(LocalDate.of(2025, 02, 17), "Caetano Veloso", 500000, 15000, lotesIngressos, true);
-		int numeroIngressos = 500;
-		int totalIngressosVip = 0;
-		int totalIngressosNormal = 0;
-		
-		show.distribuiIngressos(numeroIngressos);
-		
-		for (Lote lote : this.lotesIngressos.values()) {
-			for (Ingresso ingresso : lote.getIngressos()) {
-				if (ingresso.getTipo() == TipoIngresso.NORMAL) {
-					totalIngressosNormal += 1;
-				} else if (ingresso.getTipo() == TipoIngresso.VIP) {
-					totalIngressosVip += 1;
-				}
-			}
+		@BeforeEach
+		void initLoteIngressos() {
+			this.lotesIngressos = new HashMap<>();
+			
+			this.lotesIngressos.put(1, new Lote(1, 0.25));
 		}
 		
-		assertTrue(totalIngressosVip >= (0.2 * numeroIngressos) && totalIngressosVip <= (0.3 * numeroIngressos));
-        assertTrue(totalIngressosNormal == (numeroIngressos - totalIngressosVip - (0.1 * numeroIngressos)));
-	}
-	
-	@Test
-	void testDistribuiIngressosMeiaEntrada() {
-		Show show = new Show(LocalDate.of(2024, 03, 03), "Ariana Grande", 600000, 40000, lotesIngressos, true);
-		int numeroIngressos = 1500;
-		int totalIngressosMeiaEntrada = 0;
-		
-		show.distribuiIngressos(numeroIngressos);
-		
-		for (Lote lote : this.lotesIngressos.values()) {
-			for (Ingresso ingresso : lote.getIngressos()) {
-				if (ingresso.getTipo() == TipoIngresso.MEIA_ENTRADA) {
-					totalIngressosMeiaEntrada += 1;
+		@Test
+		void testDistribuiIngressosVip() {
+			Show show = new Show(LocalDate.of(2025, 02, 17), "Caetano Veloso", 500000, 15000, lotesIngressos, true);
+			int numeroIngressos = 500;
+			int totalIngressosVip = 0;
+			int totalIngressosNormal = 0;
+			
+			show.distribuiIngressos(numeroIngressos);
+			
+			for (Lote lote : this.lotesIngressos.values()) {
+				for (Ingresso ingresso : lote.getIngressos()) {
+					if (ingresso.getTipo() == TipoIngresso.NORMAL) {
+						totalIngressosNormal += 1;
+					} else if (ingresso.getTipo() == TipoIngresso.VIP) {
+						totalIngressosVip += 1;
+					}
 				}
 			}
+			
+			assertTrue(totalIngressosVip >= (0.2 * numeroIngressos) && totalIngressosVip <= (0.3 * numeroIngressos));
+	        assertTrue(totalIngressosNormal == (numeroIngressos - totalIngressosVip - (0.1 * numeroIngressos)));
 		}
 		
-		assertTrue(totalIngressosMeiaEntrada == (0.1 * numeroIngressos));
-	}
-	
-	@Test
-	void testDistribuiIngressos() {
-		Show show = new Show(LocalDate.of(2025, 01, 10), "Djavan", 700000, 20000, lotesIngressos, true);
-		int numeroIngressos = 3000;
-		int totalIngressosVip = 0;
-		int totalIngressosNormal = 0;
-		int totalIngressosMeiaEntrada = 0;
-		
-		show.distribuiIngressos(numeroIngressos);
-		
-		for (Lote lote : this.lotesIngressos.values()) {
-			for (Ingresso ingresso : lote.getIngressos()) {
-				if (ingresso.getTipo() == TipoIngresso.NORMAL) {
-					totalIngressosNormal += 1;
-				} else if (ingresso.getTipo() == TipoIngresso.VIP) {
-					totalIngressosVip += 1;
-				} else {
-					totalIngressosMeiaEntrada += 1;
+		@Test
+		void testDistribuiIngressosMeiaEntrada() {
+			Show show = new Show(LocalDate.of(2024, 03, 03), "Ariana Grande", 600000, 40000, lotesIngressos, true);
+			int numeroIngressos = 1500;
+			int totalIngressosMeiaEntrada = 0;
+			
+			show.distribuiIngressos(numeroIngressos);
+			
+			for (Lote lote : this.lotesIngressos.values()) {
+				for (Ingresso ingresso : lote.getIngressos()) {
+					if (ingresso.getTipo() == TipoIngresso.MEIA_ENTRADA) {
+						totalIngressosMeiaEntrada += 1;
+					}
 				}
 			}
+			
+			assertTrue(totalIngressosMeiaEntrada == (0.1 * numeroIngressos));
+		}
+	}
+	
+	@Nested
+	class ShowFunctionalTestParticaoEquivalencia {
+		private Map<Integer, Lote> lotesIngressos;
+		
+		@BeforeEach
+		void initLoteIngressos() {
+			this.lotesIngressos = new HashMap<>();
+			
+			this.lotesIngressos.put(1, new Lote(1, 0.25));
 		}
 		
-		assertTrue(totalIngressosVip >= (0.2 * numeroIngressos) && totalIngressosVip <= (0.3 * numeroIngressos));
-		assertTrue(totalIngressosMeiaEntrada == (0.1 * numeroIngressos));
-        assertTrue(totalIngressosNormal >= (0.6 * numeroIngressos) && totalIngressosVip <= (0.7 * numeroIngressos));
+		@Test
+		void testDistribuiIngressos() {
+			Show show = new Show(LocalDate.of(2025, 01, 10), "Djavan", 700000, 20000, lotesIngressos, true);
+			int numeroIngressos = 3000;
+			int totalIngressosVip = 0;
+			int totalIngressosNormal = 0;
+			int totalIngressosMeiaEntrada = 0;
+			
+			show.distribuiIngressos(numeroIngressos);
+			
+			for (Lote lote : this.lotesIngressos.values()) {
+				for (Ingresso ingresso : lote.getIngressos()) {
+					if (ingresso.getTipo() == TipoIngresso.NORMAL) {
+						totalIngressosNormal += 1;
+					} else if (ingresso.getTipo() == TipoIngresso.VIP) {
+						totalIngressosVip += 1;
+					} else {
+						totalIngressosMeiaEntrada += 1;
+					}
+				}
+			}
+			
+			assertTrue(totalIngressosVip >= (0.2 * numeroIngressos) && totalIngressosVip <= (0.3 * numeroIngressos));
+			assertTrue(totalIngressosMeiaEntrada == (0.1 * numeroIngressos));
+	        assertTrue(totalIngressosNormal >= (0.6 * numeroIngressos) && totalIngressosVip <= (0.7 * numeroIngressos));
+		}
 	}
 
 }
